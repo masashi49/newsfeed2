ba4190c0b39433ab21343e3774f7e3d37cccdb34
yamadamasashi <shisama94@gmail.com>
2023-11-27 09:52:40 +0900

    fragmentを分割する

--- relay-examples/newsfeed/src/components/Newsfeed.tsx
+++ relay-examples/newsfeed/src/components/Newsfeed.tsx
@@ -1,20 +1,17 @@
 import * as React from "react";
 import Story from "./Story";
+import { graphql } from "relay-runtime";
+import { useLazyLoadQuery } from "react-relay";
+import type {NewsfeedQuery as NewsfeedQueryType} from './__generated__/NewsfeedQuery.graphql';
+
 
 export default function Newsfeed() {
-  const story = {
-    title: "Placeholder Story",
-    summary: "Placeholder data, to be replaced with data fetched via GraphQL",
-    poster: {
-      name: "Placeholder Person",
-      profilePicture: {
-        url: "/assets/cat_avatar.png",
-      },
-    },
-    thumbnail: {
-      url: "/assets/placeholder.jpeg",
-    },
-  };
+  const data = useLazyLoadQuery<NewsfeedQueryType> (
+    NewsfeedQuery,{}
+  )
+  const story = data.topStory
+
+  console.log(story)
 
   return (
     <div className="newsfeed">
@@ -22,3 +19,19 @@ export default function Newsfeed() {
     </div>
   );
 }
+
+// GraphQlをjsに埋め込むには 「graphql``」といった文字リテラルが必要。
+// Relay コンパイラーはコード内でこの記述を検索し、コンパイルできるようになる。
+// query NewsfeedQuery クエリの宣言。
+// 
+const NewsfeedQuery = graphql`
+ query NewsfeedQuery{
+  topStory {
+    ...StoryFragment 
+  }
+ }
+`
+
+//topStoryの中身をStory.tsxに押し込んだ。
+//NewsfeedQueryはStoryFragmentのフラグメントを探し出すことに徹する.
+//StoryFragmentの中身がどうかわろうと、NewsfeedQueryには影響しない。
--- relay-examples/newsfeed/src/components/Story.tsx
+++ relay-examples/newsfeed/src/components/Story.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import Card from "./Card";
+import { graphql } from "relay-runtime";
 import Heading from "./Heading";
 import PosterByline, { type Props as PosterBylineProps } from "./PosterByline";
 import StorySummary from "./StorySummary";
 import Image from "./Image";
+import Timestamp from "./Timestamp";
 
 type Props = {
   story: {
     title: string;
     summary: string;
+    createAt: string;
     thumbnail: {
       url: string;
     };
@@ -21,8 +24,27 @@ export default function Story({ story }: Props): React.ReactElement {
     <Card>
       <PosterByline poster={story.poster} />
       <Heading>{story.title}</Heading>
+      <Timestamp time={story.createdAt} />
       <Image image={story.thumbnail} width={400} height={400} />
       <StorySummary summary={story.summary} />
     </Card>
   );
 }
+
+const StoryFragment = graphql`
+fragment StoryFragment on Story{
+    title
+    summary
+    createdAt
+    poster {
+      name
+      profilePicture {
+        url
+      }
+    }
+    thumbnail {
+      url
+    }
+  }
+
+`
